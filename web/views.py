7e85a4191ca06351eb7c9102fa67eef8cc5be087
Nuclear451 <Koryakovsky.Ivan@gmail.com>
2019-05-14 23:18:05 +0500

    simple page

--- web/views.py
+++ web/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from .models import Post
+
+
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.order_by('post_date')
+    return render(request, 'web/index.html', {'posts': posts})
+
+
